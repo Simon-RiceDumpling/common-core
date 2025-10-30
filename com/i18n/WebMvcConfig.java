3776dddd12087e7d8199a2519fb121284577e127
renbo <simon@atlasmobi.com>
2025-10-30 10:57:04 +0800

    1

--- com/i18n/WebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.i18n;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-//@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-
-}
---
+++ src/main/java/com/utils/Demo.java
@@ -0,0 +1,17 @@
+package com.utils;
+
+import lombok.Data;
+
+/**
+ * @program: common-core
+ * @description: TODO
+ * @author: renBo
+ * @create: 2025-10-30 10:53
+ **/
+@Data
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
