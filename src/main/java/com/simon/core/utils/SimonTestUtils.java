829adfbcdc56ed654c9aaf48bcfa03c85ae8e3c4
renbo <simon@atlasmobi.com>
2025-10-30 11:40:31 +0800

    1

---
+++ src/main/java/com/simon/core/utils/SimonTestUtils.java
@@ -0,0 +1,15 @@
+package com.simon.core.utils;
+
+/**
+ * @program: common-core
+ * @description: TODO
+ * @author: renBo
+ * @create: 2025-10-30 11:40
+ **/
+public class SimonTestUtils {
+
+
+    public static String test() {
+        return "SIMON";
+    }
+}
